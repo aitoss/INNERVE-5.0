61a83518f50b2135e6b77eccaac3db61693b614f
satya9500 <satya.prakash9500@gmail.com>
2021-01-29 20:17:47 +0530

    UI fixes

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const addNavActive = () => {
 }
 
 // handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#nav"]', function(e) {
+$(document).on('click', 'a[href^="#nav"]', function (e) {
     // target element id
     var id = $(this).attr('href');
 
@@ -28,49 +28,6 @@ $(document).on('click', 'a[href^="#nav"]', function(e) {
     $('html,body').animate({ scrollTop: pos });
 });
 
-const findBrowser = () => {
-    // Opera 8.0+
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-
-    // Safari 3.0+ "[object HTMLElementConstructor]"
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function(p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-
-    // Internet Explorer 6-11
-    var isIE = /*@cc_on!@*/ false || !!document.documentMode;
-
-    // Edge 20+
-    var isEdge = !isIE && !!window.StyleMedia;
-
-    // Chrome 1 - 79
-    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-    // Edge (based on chromium) detection
-    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
-
-    // Blink engine detection
-    var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-
-    var output = 'Detecting browsers by ducktyping:<hr>';
-    output += 'isFirefox: ' + isFirefox + '<br>';
-    output += 'isChrome: ' + isChrome + '<br>';
-    output += 'isSafari: ' + isSafari + '<br>';
-    output += 'isOpera: ' + isOpera + '<br>';
-    output += 'isIE: ' + isIE + '<br>';
-    output += 'isEdge: ' + isEdge + '<br>';
-    output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
-    output += 'isBlink: ' + isBlink + '<br>';
-    console.log(output);
-    if (isFirefox) {
-        body.classList.add("black-background");
-    }
-}
-
-findBrowser();
-
 
 // window.onscroll = function() { scrollFunction() };
 
@@ -84,14 +41,14 @@ findBrowser();
 //     }
 // }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
-    document.getElementById("fav-logo-innerve").style.transform = "scale(1.3)";
-  } else {
-    document.getElementById("fav-logo-innerve").style.transform = "scale(0.9)";
-  }
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("fav-logo-innerve").style.transform = "scale(1.3)";
+    } else {
+        document.getElementById("fav-logo-innerve").style.transform = "scale(0.9)";
+    }
 }
 
 // countdown
