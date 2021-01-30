9af4fb5cf7769e8595a442b9086e046059800e1e
xerycks <xerycks@gmail.com>
2021-01-30 13:35:44 +0530

    snow removal, adding particles

--- js/snow.js
+++ js/snow.js
@@ -1,41 +1,68 @@
-"use strict";
-
-window.isMobile = function () {
-    var check = false;
-    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
+var Stars = {
+    canvas: null,
+    context: null,
+    circleArray: [],
+    colorArray: ["#909bd9", "#3b5a9b", "#006599", "#2b97f1", "#e84713", "#001b47", "#007bff", "#ffffff", "#4d5664d8"],
+    mouseDistance: 50,
+    radius: 0.7,
+    maxRadius: 1.3,
+    mouse: { x: void 0, y: void 0, down: !1, move: !1 },
+    init: function () {
+        (this.canvas = document.getElementById("stars")),
+            (this.canvas.width = window.innerWidth),
+            (this.canvas.height = window.innerHeight),
+            (this.canvas.style.display = "block"),
+            (this.context = this.canvas.getContext("2d")),
+            window.addEventListener("mousemove", this.mouseMove),
+            window.addEventListener("resize", this.resize),
+            this.prepare(),
+            this.animate();
+    },
+    Circle: function (e, t, n, i, o, r) {
+        (this.x = e),
+            (this.y = t),
+            (this.dx = n),
+            (this.dy = i),
+            (this.radius = o),
+            (this.minRadius = this.radius),
+            (this.draw = function () {
+                Stars.context.beginPath(), Stars.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, !1), (Stars.context.fillStyle = r), Stars.context.fill()
+            }),
+            (this.update = function () {
+                (this.x + this.radius > Stars.canvas.width || this.x - this.radius < 0) && (this.dx = -this.dx),
+                    (this.y + this.radius > Stars.canvas.height * 2 || this.y - this.radius < 0) && (this.dy = -this.dy),
+                    (this.x += this.dx / 3),
+                    (this.y += this.dy / 3),
+                    Stars.mouse.x - this.x < Stars.mouseDistance && Stars.mouse.x - this.x > -Stars.mouseDistance && Stars.mouse.y - this.y < Stars.mouseDistance && Stars.mouse.y - this.y > -Stars.mouseDistance
+                        ? this.radius < Stars.maxRadius && (this.radius += 1.3)
+                        : this.radius > this.minRadius && (this.radius -= 1.3),
+                    this.draw();
+            });
+    },
+    prepare: function () {
+        this.circleArray = [];
+        for (var e = 0; e < particleNumber; e++) {
+            var t = Stars.radius,
+                n = Math.random() * (this.canvas.width - 2 * t) + t,
+                i = Math.random() * (this.canvas.height - 2 * t) + t,
+                o = 1.5 * (Math.random() - 0.5),
+                r = 1.5 * (Math.random() - 1),
+                a = this.colorArray[Math.floor(Math.random() * this.colorArray.length)];
+            this.circleArray.push(new this.Circle(n, i, o, r, t, a));
+        }
+    },
+    animate: function () {
+        requestAnimationFrame(Stars.animate), Stars.context.clearRect(0, 0, Stars.canvas.width, Stars.canvas.height);
+        for (var e = 0; e < Stars.circleArray.length; e++) {
+            Stars.circleArray[e].update();
+        }
+    },
+    mouseMove: function (e) {
+        (Stars.mouse.x = e.x), (Stars.mouse.y = e.y);
+    },
+    resize: function () {
+        (Stars.canvas.width = window.innerWidth), (Stars.canvas.height = window.innerHeight);
+    },
 }
 
-function letItSnow(flakes, target) {
-    var d = document;
-    var s = d.createElement('style');
-    s.type = 'text/css';
-    s.innerHTML = ".snowy-sky{z-index:-100;overflow:hidden;position:absolute;left:0;top:0;width:100%;height:100%}.snow-flake{position:absolute;filter: blur(0.5px);background-color:#fff;animation:snowy 10s linear infinite}@keyframes snowy{0%{transform:translateX(-500%);top:-20%}100%{transform:translateX(500%);top:100%}}";
-    d.getElementsByTagName('head')[0].appendChild(s);
-    var k = d.createElement('div');
-    k.classList.add('snowy-sky');
-    target ? target.appendChild(k) : d.body.appendChild(k);
-
-    function r(v) {
-        var c = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-        return Math.floor(Math.random() * v) + c;
-    }
-
-    for (var i = 0; i < flakes; i++) {
-        var f = d.createElement('div');
-        f.classList.add('snow-flake');
-        var w = "".concat(r(3.5, 3.5), "px");
-        var fs = f.style;
-        fs.width = w;
-        fs.height = w;
-        fs.borderRadius = w;
-        fs.opacity = r(5, 2) * 0.1;
-        fs.left = "".concat(r(100), "%");
-        fs.animationDuration = "".concat(r(flakes, 50) * 0.06, "s");
-        k.appendChild(f);
-    }
-}
-
-if (!isMobile()) {
-    letItSnow(200)
-}
+Stars.init();
