f34161f44f60a003c4da910adaaafdf20d36a2fa
kazukilapislazuli <kazukilapislazuli@gmail.com>
2021-02-09 19:57:51 +0530

    reminder removed

--- js/app.js
+++ js/app.js
@@ -95,14 +95,9 @@ $(document).ready(function () {
 
     // process the form
     $('form').submit(function (event) {
-
-        // get the form data
-        // there are many ways to get this data using jQuery (you can use the class or id also)
         var formData = {
             'email': $('input[name=email]').val(),
         };
-
-        // process the form
         $.ajax({
             type: 'POST',
             url: 'https://aitoss.club/api/v1/innerveReminder',
