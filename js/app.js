301f01d698b43e7141f8d651301dc5b3b4f58516
kazukilapislazuli <kazukilapislazuli@gmail.com>
2021-03-10 20:56:59 +0530

    timer stopped

--- js/app.js
+++ js/app.js
@@ -63,24 +63,24 @@ function scrollFunction() {
 
 // countdown
 
-var InnerveDate = new Date("Mar 6,2021 09:00:00").getTime();
+// var InnerveDate = new Date("Mar 6,2021 09:00:00").getTime();
 
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var timeLeft = InnerveDate - now;
-    // console.log(timeLeft, InnerveDate, now);
+// var x = setInterval(function () {
+//     var now = new Date().getTime();
+//     var timeLeft = InnerveDate - now;
+//     // console.log(timeLeft, InnerveDate, now);
 
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
+//     var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((timeLeft % (1000 * 60)) / (1000));
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+//     document.getElementById("days").innerHTML = days;
+//     document.getElementById("hours").innerHTML = hours;
+//     document.getElementById("minutes").innerHTML = minutes;
+//     document.getElementById("seconds").innerHTML = seconds;
 
-}, 1000)
+// }, 1000)
 
 const changeFaqIcon = (id) => {
     //console.log(id);
